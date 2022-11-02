c9ec929bc1c7af5b84c09abb7cb6091d5052d31d
naoki0618 <nok.h0618@gmail.com>
2022-11-02 10:17:19 +0900

    fix

--- sns/settings.py
+++ sns/settings.py
@@ -134,16 +134,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# 追加
-STATICFILES_DIRS = (
-   os.path.join(BASE_DIR, "static"),
-)
+# # 追加
+# STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, "static"),
+# )
 # 追加 mediaを扱うための設定
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
